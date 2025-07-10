864b04966efab0bbc6143a0c00aa01dbaec9e92a
John Muriithi <johnmuriithi339@gmail.com>
2025-07-10 13:15:02 +0300

    Added missing getAuth import

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"; // ✅ REQUIRED for getAuth to work
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
